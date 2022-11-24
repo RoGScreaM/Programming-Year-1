67d79512c853eb8a3022e47bf57f142e34597a12
RoGScreaM <102172592+RoGScreaM@users.noreply.github.com>
2022-11-24 19:32:10 +0000

    Add files via upload

---
+++ WhileEx-1/WhileEx-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileEx_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****3 DIGIT NUMBER*****");
+
+            int num = 0;
+
+            do
+            {
+                Console.Write("Enter 3 digit number: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num < 100 || num > 999)
+                {
+
+                    Console.WriteLine("Invalid number press any key to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            while (num < 100 || num > 999);
+            Console.WriteLine("\n\nCorrect your number is {0}", num);
+            Console.WriteLine("I am out of loop");
+
+            Console.ReadLine();
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
